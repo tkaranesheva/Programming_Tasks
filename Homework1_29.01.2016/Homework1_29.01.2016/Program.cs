040dbd98803829db67d3b2f8628ea8bf7b9402a0
tkaranesheva <tkaranesheva@gmail.com>
2016-02-29 21:43:36 +0200

    hello world

---
+++ Homework1_29.01.2016/Homework1_29.01.2016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_29._01._2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my first program!");
+        }
+    }
+}
