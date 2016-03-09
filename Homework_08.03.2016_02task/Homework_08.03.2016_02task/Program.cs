9f831155fc81da7380d436795558a911ccd7a680
tkaranesheva <tkaranesheva@gmail.com>
2016-03-09 21:55:40 +0200

    work

---
+++ Homework_08.03.2016_02task/Homework_08.03.2016_02task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_08._03._2016_02task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your result:");
+            int numInt = int.Parse(Console.ReadLine());
+
+            if (numInt >= 1 && numInt <= 9)
+            {
+                Console.WriteLine("The number is in the interval [1;9]");
+            }
+            if (numInt >= 1 && numInt <= 3)
+            {
+                Console.WriteLine(numInt * 5);
+            }
+            if (numInt >= 4 && numInt <= 6)
+            {
+                Console.WriteLine(numInt * 10);
+            }
+            if (numInt >= 7 && numInt <= 9)
+            {
+                Console.WriteLine(numInt * 50);
+            }
+            else
+            {
+                Console.WriteLine("The number is out of the interval");
+            }
+        }
+    }
+    
+}
---
+++ uprajnenie_08.03/uprajnenie_08.03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uprajnenie_08._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int numInt = int.Parse(Console.ReadLine());
+
+            if ((numInt >=1) && (numInt<=5))
+            {
+                if (numInt % 2 == 0)
+                {
+                    Console.WriteLine("Chetno");
+                }
+                else
+                {
+                    Console.WriteLine("Nechetno");
+                }
+                if ((numInt >=6)&&(numInt<=15))
+                {
+                    if (numInt%3==0)
+                    {
+                        Console.WriteLine("Deli se na 3");
+                        }
+              if ((numInt<1)&& (numInt>15))
+
+                }
+            }
+        }
+    }
