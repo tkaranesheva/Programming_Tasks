91d8191cd6490f576fd4273f0fc4cc027cbc89db
tkaranesheva <tkaranesheva@gmail.com>
2016-03-10 18:43:17 +0200

    task3

---
+++ Task3_08.03.2016/Task3_08.03.2016/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_08._03._2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a card: ");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "10":
+                case "J":
+                case "Q":
+                case "K":
+                case "A":
+                    Console.WriteLine("Valid card");
+                    break;
+                default:
+                    Console.WriteLine("Invalid card");
+                    break;
+                 }
+            }
+    }
+}
